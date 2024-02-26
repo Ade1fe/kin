ae79ea91853ee17fc103d7902126ed4d1f0bfb59
Ade1fe <addypearl09@gmail.com>
2024-02-26 19:43:52 +0100

    index.html

--- src/common/components/built/index.tsx
+++ src/common/components/built/index.tsx
@@ -54,24 +54,29 @@ interface DetailsPageProps {
 
 <Box mt={['50px','60px','70px']} display="grid" gridTemplateColumns={['repeat(auto-fit, minmax(200px, 1fr))','repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)', 'repeat(2, 1fr)']} gridTemplateRows={['auto', 'auto', 'auto']} gap={['30px', '40px', '20px']}>
 <Box display={['grid', 'grid', "flex"]} gap='10px' alignItems='center'  _hover={{ transform: 'scale(0.9)', transition: 'transform 0.3s ease' }}>
-        <Image src={brand5}  w={['full','full', '200px','200px','250px']} h={['300px','300px', 'auto']}  />
-    
-
-    <Box className="">
+     
+<Box className="">
         <Text fontWeight='600' fontSize={['16px']}>Customised Debit Cards</Text>
         <Text fontSize={['14px']}>Elevate your home shopping experience with debit cards for you and your loved ones. Fund your card with only what you need and track all your card expenses seamlessly.</Text>
     </Box>
+    
+        <Image src={brand5}  w={['full','full', '200px','200px','250px']} h={['300px','300px', 'auto']}  />
+    
+
+  
     </Box>
 
 
     <Box display={['grid', 'grid', "flex"]} gap='10px' alignItems='center'  _hover={{ transform: 'scale(0.9)', transition: 'transform 0.3s ease' }}>
-        <Image src={brand6} w={['full','full', '200px','200px','250px']}h={['300px','300px', 'auto']}  />
-    
-
+       
     <Box className="">
         <Text fontWeight='600' fontSize={['16px']}>Health and Wealth Growth</Text>
         <Text fontSize={['14px']}>Get care, comfort, and protection together with our ever-growing list of value-added services like insurance, investments, savings, and healthcare and lots more!</Text>
     </Box>
+        <Image src={brand6} w={['full','full', '200px','200px','250px']}h={['300px','300px', 'auto']}  />
+    
+
+   
     </Box>
 </Box>
 
--- src/common/components/footer/index.tsx
+++ src/common/components/footer/index.tsx
@@ -5,9 +5,9 @@ import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <Box px={['20px', '35px','50px', '50px','70px']} my='30px' display='flex' gap='6' justifyContent='space-between' alignItems='start'>
-       <Image src={Logo} w={['50px']} />
-       <Box display="flex" gap='4' color='#5C6370'>
+    <Box px={['20px', '35px','50px', '50px','70px']} my='30px' display={['block', 'flex']} gap='6' justifyContent='space-between' alignItems='start'>
+       <Image src={Logo} w={['50px']} mx={['auto', '0' ]} mb={['20px', '0']} />
+       <Box display="flex" gap='4' mx={['auto', '0' ]}  justifyContent={['center' , 'flex-end']} textAlign={['center']} color='#5C6370'>
         <Icon as={FaFacebookF} boxSize='5' />
         <Icon as={FaInstagram} boxSize='5' />
         <Icon as={FaTwitter} boxSize='5' />
